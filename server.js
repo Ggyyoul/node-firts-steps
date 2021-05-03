79dee166a634383d4aa96babb1aa961e5c6c040b
Juan Palacios <juan980424@gmail.com>
2021-05-03 10:45:06 -0500

    como utilizar el router

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
-//mport express from 'express';
+const router = express.Router(); //buscar en la documentacion
 
 let app = express();
 
-app.use("/", function (req, res) {
-  res.send("hola");
+app.use(router);
+
+router.get("/message", (req, res) => {
+  res.send("lista de mensajes");
 });
 
+router.post("/message", (req, res) => {
+  res.send("mensaje agregado");
+});
 app.listen(3000);
 console.log("la app esta escuchando en http://localhost:3000");
