86ae41c7609bb7382811f3421aa90cac455f0157
Juan Palacios <juan980424@gmail.com>
2021-05-03 16:31:12 -0500

    recibir informacion desde el cliente body y query

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const router = express.Router(); //buscar en la documentacion
 
 let app = express();
 
-app.use(router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router); //el ruter debe estar al final
 
 router.get("/message", (req, res) => {
   res.send("lista de mensajes");
 });
 
 router.post("/message", (req, res) => {
+  console.log(req.body);
+  console.log(req.query);
+  console.log(req.body.text);
+
   res.send("mensaje agregado");
 });
 app.listen(3000);
