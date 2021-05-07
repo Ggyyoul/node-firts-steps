3bc13522583a28e8269f3abd9d0963abcd7b23d3
Juan Palacios <juan980424@gmail.com>
2021-05-07 08:48:08 -0500

    almacenar y leer datos en  mongodb

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,13 +1,22 @@
-// mock == falsear la base de datos
+const db = require("mongoose");
+const Model = require("./model");
+db.Promise = global.Promise; //utilice las promesas nativas de node (clase nativa de node)
+db.connect(
+  "mongodb://db_user:123abc@cluster0-shard-00-00.hochh.mongodb.net:27017,cluster0-shard-00-01.hochh.mongodb.net:27017,cluster0-shard-00-02.hochh.mongodb.net:27017/telegram?ssl=true&replicaSet=atlas-lnnmdu-shard-0&authSource=admin&retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-const list = [];
+console.log("[db] conectada con exito");
 
 function addMessages(message) {
-  list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  // return list;
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
